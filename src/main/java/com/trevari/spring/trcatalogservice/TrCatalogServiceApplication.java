53ba0abb63b6f0cd493221d332125fae0ae0a268
DongWoonKim <01030985843z@naver.com>
2025-08-16 11:06:01 +0900

    chore: initial prj

---
+++ src/main/java/com/trevari/spring/trcatalogservice/TrCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.trevari.spring.trcatalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrCatalogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrCatalogServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/trevari/spring/trcatalogservice/TrCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trevari.spring.trcatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrCatalogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
